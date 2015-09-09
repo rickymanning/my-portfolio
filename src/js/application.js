abf68db1a6edb58b52671154266e881bbf261aac
Ricky Manning <rlcm@bts.co.uk>
2015-09-09 19:01:58 +0100

    add id to navigation and use JS (jQuery) to toggle its visibility upon hamburger clicks

--- src/js/application.js
+++ src/js/application.js
@@ -1,1 +1,9 @@
-"use strict";
+$(document).ready(function () {
+    "use strict";
+    
+    var $icon = $('#main-menu-icon');
+    $icon.on('click', function(event) {
+        event.preventDefault();
+        $('#main-menu').toggle();
+    });
+});
