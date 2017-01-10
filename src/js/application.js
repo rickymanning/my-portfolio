c8401db02e867b2ef3240d93d5ac371d407a189b
Ricky Manning <rlcm@bts.co.uk>
2017-01-10 11:42:02 +0000

    replace tabs with spaces in files I've written

--- src/js/application.js
+++ src/js/application.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     "use strict";
-    
-    // we assume that javascript isn't running and consequently display the 
+
+    // we assume that javascript isn't running and consequently display the
     // navigation by default and hide the menu icon in our css. if we get in
     // here then javascript must be running, so we show the icon, hide the
     // navigation and attach an event handler to the menu icon's click event
-    // so that we can toggle visibility of the menu links. 
+    // so that we can toggle visibility of the menu links.
     var $icon = $('#main-menu-icon');
     $icon.removeClass('hidden');
     $icon.on('click', function(event) {
         event.preventDefault();
         $('#main-menu').animate({height: 'toggle'}, 500);
     });
-                
+
     //hide the navigation
     $('#main-menu').hide();
 
